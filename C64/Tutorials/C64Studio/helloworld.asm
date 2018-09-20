25a1e74e8610cea91b334869565168623b9bc785
Bert Heesakkers <heesakkers.b@nhtv.nl>
2018-09-20 08:01:16 +0200

    [C64]
    - update hello world

--- C64/Tutorials/C64Studio/helloworld.asm
+++ C64/Tutorials/C64Studio/helloworld.asm
@@ -5,10 +5,11 @@
 !byte $0A,$00         ; line number
 !byte $9E             ; Kernal SYS command
 !byte $20             ; space
+!byte $28             ; opening bracket
 !byte $32,$30,$36,$34 ; start address in ascii (hex) [2064 = $0810]
+!byte $29             ; closing bracket
 !byte $00             ; end of line
 !byte $00,$00         ; end of program
-!byte $00,$00         ; padding to make sure actual code starts at correct adress
 
   jsr $e544           ; call Kernal (no typo) subroutine to clear the screen
   ldx #$00            ; load literal value 0 into the X register
